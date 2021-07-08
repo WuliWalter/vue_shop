572c89d55e51defe474431991d4489dadad90533
Ming <15996164332@163.com>
2021-07-08 23:40:32 +0800

    报表完成

---
+++ src/components/report/Report.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div id="main" style="width: 600px;height:400px;"></div>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+import _ from 'lodash'
+export default {
+  data() {
+    return {
+      options: {
+        title: {
+          text: '用户来源'
+        },
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'cross',
+            label: {
+              backgroundColor: '#E9EEF3'
+            }
+          }
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: [
+          {
+            boundaryGap: false
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value'
+          }
+        ]
+      }
+    }
+  },
+  async mounted() {
+    var myChart = echarts.init(document.querySelector('#main'))
+
+    const { data: res } = await this.$http.get('reports/type/1')
+
+    if (res.meta.status !== 200) {
+      return this.$message.error('获取折线图数据失败！')
+    }
+
+    const result = _.merge(res.data, this.options)
+    myChart.setOption(result)
+  },
+  methods: {}
+}
+</script>
+<style lang="less" scoped></style>
